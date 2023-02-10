a789b26b3e9a2b6ccb50800baaa9f765795d5ba4
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-02-10 19:37:28 +0100

    controller methods

--- src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/controller/ChannelController.java
+++ src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/controller/ChannelController.java
@@ -1,11 +1,16 @@
 package com.budgetducklingsinc.websocketsinlamningsuppgift.controller;
 
+import com.budgetducklingsinc.websocketsinlamningsuppgift.model.ChatRoom;
 import com.budgetducklingsinc.websocketsinlamningsuppgift.model.PermanentChannel;
 import com.budgetducklingsinc.websocketsinlamningsuppgift.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,47 +19,82 @@ public class ChannelController {
     @Autowired
     private ChannelService channelService;
 
-    @GetMapping("/permanentChannels/{permanentChannelId}/chatRooms")
-    public ResponseEntity<List<PermanentChannel>> getChannels() {
-        List<PermanentChannel> channels = channelService.getAllChatRooms();
-
-        if (channels.isEmpty()) {
-            return ResponseEntity
-                    .status(204)
-                    .header("x-information", "No data was found in the database")
-                    .build();
-        } else {
-            return ResponseEntity.ok(channels);
-        }
+    @GetMapping("/permanentChannels/{permanentChannelId}")
+    public PermanentChannel getPermanentChannelById (@PathVariable String permanentChannelId) {
+        PermanentChannel permanentChannel = channelService.getPermanentChannelById(permanentChannelId);
+        if(permanentChannel == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            return permanentChannel;
     }
 
+    @GetMapping("/permanentChannels/{permanentChannelId}/chatRooms")
+        public List<ChatRoom> getAllChatRooms(@PathVariable String permanentChannelId) {
+        List<ChatRoom> chatRooms = channelService.getAllChatRooms(permanentChannelId);
+        if(chatRooms == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return chatRooms;
+    }
+
+
+
+//    @GetMapping("/permanentChannels/{permanentChannelId}/chatRooms")
+//    public ResponseEntity<List<PermanentChannel>> getChannels() {
+//        List<PermanentChannel> channels = channelService.getAllChatRooms();
+//
+//        if (channels.isEmpty()) {
+//            return ResponseEntity
+//                    .status(204)
+//                    .header("x-information", "No data was found in the database")
+//                    .build();
+//        } else {
+//            return ResponseEntity.ok(channels);
+//        }
+//    }
+
     @GetMapping("/permanentChannels/{permanentChannelId}/chatRooms/{chatRoomTitle}")
-    public ResponseEntity<PermanentChannel> getChannel(@PathVariable String title) {
-        return ResponseEntity.ok(channelService.getChannelByTitle(title));
+    public ChatRoom getChatRoomByTitle(@PathVariable String permanentChannelId,
+                                                               @PathVariable String chatRoomTitle) {
+        ChatRoom chatRoom = channelService.getChatRoomByTitle(permanentChannelId, chatRoomTitle);
+        if(chatRoom == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return chatRoom;
     }
 
     @GetMapping("/permanentChannels/{permanentChannelId}/chatRooms/{chatRoomId}")
-    public ResponseEntity<PermanentChannel> getChannel(@PathVariable("id") long id) {
-        return ResponseEntity.ok(channelService.getChannelById(id));
+    public ChatRoom getChatRoomById(@PathVariable String permanentChannelId,
+                                                            @PathVariable String chatRoomId) {
+        ChatRoom chatRoom = channelService.getChatRoomById(permanentChannelId, chatRoomId);
+        if(chatRoom == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return chatRoom;
     }
 
     @PostMapping("/permanentChannels/{permanentChannelId}/chatRooms")
-    public ResponseEntity<List<PermanentChannel>> createChatRoom(@PathVariable String permanentChannelId, @RequestBody ChatRoom chatRoom) {
-        channelService.createChatRoom(permanentChannelId, chatRoom);
-        List<PermanentChannel> chatRooms = channelService.getAllChatRooms();
-        return ResponseEntity.status(201).body(chatRooms);
+    public ResponseEntity<Object> createChatRoom(@PathVariable String permanentChannelId, @RequestBody ChatRoom chatRoom) {
+        String chatRoomId = channelService.createChatRoom(permanentChannelId, chatRoom);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/chatRoomId")
+                .buildAndExpand(chatRoomId).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
-    @DeleteMapping("/permanentChannels/{permanentChannelId}/chatRooms/{chatRoomId}")
-    public ResponseEntity<PermanentChannel> deleteChatRoom(@PathVariable String chatRoomId) {
-        channelService.deleteChatRoom(chatRoomId);
+//    @PostMapping("/permanentChannels/{permanentChannelId}/chatRooms")
+//    public ResponseEntity<List<PermanentChannel>> createChatRoom(@PathVariable String permanentChannelId, @RequestBody ChatRoom chatRoom) {
+//        channelService.createChatRoom(permanentChannelId, chatRoom);
+//        List<PermanentChannel> chatRooms = channelService.getAllChatRooms();
+//        return ResponseEntity.status(201).body(chatRooms);
+//    }
 
-        if (chatRoomId > chatRooms.size()) {
-            return ResponseEntity
-                    .status(400)
-                    .header("x-error-msg", "Id is out of bounds, course does not exist with such id")
-                    .build();
-        }
+    @DeleteMapping("/permanentChannels/{permanentChannelId}/chatRooms/{chatRoomId}")
+    public ResponseEntity<Object> deleteChatRoom(@PathVariable String permanentChannelId, @PathVariable String chatRoomId) {
+        channelService.deleteChatRoom(permanentChannelId, chatRoomId);
+
+//        if (chatRoomId > chatRooms.size()) {
+//            return ResponseEntity
+//                    .status(400)
+//                    .header("x-error-msg", "Id is out of bounds, course does not exist with such id")
+//                    .build();
+//        }
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/service/ChannelService.java
+++ src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/service/ChannelService.java
@@ -1,36 +1,67 @@
 package com.budgetducklingsinc.websocketsinlamningsuppgift.service;
 
+import com.budgetducklingsinc.websocketsinlamningsuppgift.model.ChatRoom;
 import com.budgetducklingsinc.websocketsinlamningsuppgift.model.PermanentChannel;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+@Service
 public class ChannelService {
 
+    private static List<PermanentChannel> permanentChannels = new ArrayList<>();
 
+    static {
+        ChatRoom chatRoom = new ChatRoom(long id, String title);
 
+        List<ChatRoom> chatRooms = new ArrayList<>(Arrays.asList(new ChatRoom(long id, String title)));
 
-    public Object getChannelByTitle(String title) {
-    }
+        PermanentChannel permanentChannel = new PermanentChannel(1, "Permanent Channel",chatRooms);
 
-    public Object getChannelById(long id) {
-    }
-
-    public String deleteChatRoom(String chatRoomId) {
-        List<chatRoom> chatRooms = getAllChatRooms();
-        if(chatRooms == null)
-            return null;
-        Predicate<? super ChatRoom> predicate = c -> c.getId().equalsIgnoreCase(chatRoom);
-        boolean removed = chatRooms.removeIf(predicate);
-        if(!removed) return null;
-        return chatRoomId;
+        permanentChannels.add(permanentChannel);
 
     }
 
-    public List<PermanentChannel> getAllChatRooms() {
+    public PermanentChannel getPermanentChannelById(String permanentChannelId) {
+    }
+
+    public List<ChatRoom> getAllChatRooms(String permanentChannelId) {
+    }
+
+    public ChatRoom getChatRoomByTitle(String permanentChannelId, String chatRoomTitle) {
+    }
+
+    public ChatRoom getChatRoomById(String permanentChannelId, String chatRoomId) {
+    }
+
+    public String createChatRoom(String permanentChannelId, ChatRoom chatRoom) {
+    }
+
+    public void deleteChatRoom(String permanentChannelId, String chatRoomId) {
     }
 
 
-
-    public void createChatRoom(String permanentChannelId, ChatRoom chatRoom) {
-    }
+//    public Object getChannelByTitle(String title) {
+//    }
+//
+//    public Object getChannelById(long id) {
+//    }
+//
+//    public String deleteChatRoom(String chatRoomId) {
+//        List<chatRoom> chatRooms = getAllChatRooms();
+//        if(chatRooms == null)
+//            return null;
+//        Predicate<? super ChatRoom> predicate = c -> c.getId().equalsIgnoreCase(chatRoom);
+//        boolean removed = chatRooms.removeIf(predicate);
+//        if(!removed) return null;
+//        return chatRoomId;
+//
+//    }
+//
+//    public List<PermanentChannel> getAllChatRooms() {
+//    }
+//    
+//    public void createChatRoom(String permanentChannelId, ChatRoom chatRoom) {
+//    }
 }
