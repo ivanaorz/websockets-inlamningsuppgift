9abe5136704ff540e3c64b90f914788ad00e5f37
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-03-15 09:32:52 +0100

    fixing bugs

--- src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/ws/ChatRoomSocketHandler.java
+++ src/main/java/com/budgetducklingsinc/websocketsinlamningsuppgift/ws/ChatRoomSocketHandler.java
@@ -1,43 +1,56 @@
 package com.budgetducklingsinc.websocketsinlamningsuppgift.ws;
 
 import com.budgetducklingsinc.websocketsinlamningsuppgift.model.ChatRoom;
+import com.budgetducklingsinc.websocketsinlamningsuppgift.service.ChannelService;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatRoomSocketHandler extends TextWebSocketHandler {
 
+    
 
     private List<WebSocketSession> webSocketSessions = new ArrayList<>();
+
 
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         for (WebSocketSession webSocketSession : webSocketSessions) {
-            String establishedSession = session.getHandshakeHeaders().getFirst("id");
-            if (establishedSession.equals(session.getId())) {
-                String messageContent = message.getPayload();
-                webSocketSession.sendMessage(new TextMessage(messageContent));
-            }
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+
+            String messageToSend = session.getHandshakeHeaders().getFirst("id");
+            broadcast(messageToSend);
         }
     }
+        public void broadcast(String messageToSend) throws IOException {
+        Gson gson = new Gson();
+
+        for (WebSocketSession webSocketSession: webSocketSessions) {
+            String textMessage = gson.toJson(messageToSend);
+            webSocketSession.sendMessage(new TextMessage(textMessage));
+
+        }
+        }
+
+
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    public void afterConnectionEstablished(WebSocketSession session) {
         webSocketSessions.add(session);
-        ChatRoom newChatRoom = new ChatRoom();
-        newChatRoom.setTitle(session.getHandshakeHeaders().getFirst("title"));
-        newChatRoom.setId(Long.valueOf(session.getHandshakeHeaders().getFirst("id")));
-        String messageContent = "New chat room created: " + newChatRoom;
-        session.sendMessage(new TextMessage(messageContent));
     }
 
+
+
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         webSocketSessions.remove(session);
     }
 }
